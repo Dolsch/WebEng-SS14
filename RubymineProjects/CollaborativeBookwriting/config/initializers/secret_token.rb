82d88ca742ddcfffb3e1471cc6c376df1fc27fa0
Dennis Olschowka <Dennis.Olschowka@smail.inf.h-brs.de>
2014-04-23 18:17:33 +0200

    Committin

---
+++ RubymineProjects/CollaborativeBookwriting/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollaborativeBookwriting::Application.config.secret_token = '74d940649e99d292978e3c9286fb49dfec5ad898f85f205228af2a475283fac70a9c308abe1344fddd00152433f6818c6b4c785434b406351301eb40ce137585'
