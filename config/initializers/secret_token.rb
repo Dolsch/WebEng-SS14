174e60af4eee771f3a931ffd91face2aecc34b7a
Bastian Mager <bastian.mager@smail.inf.h-brs.de>
2014-04-24 18:47:26 +0200

    new files for rails app with correct version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollaborativeBookWriting::Application.config.secret_token = '86f2eaadc39184de3463cfd1c10cb9ec469b1eb3aa1578a1e366c56fd7840afa36842593dffedc6efc795325f61fd404ec5a2f324c1a76f1b308d48dbb2bdbfc'
