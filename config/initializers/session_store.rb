cb3f779e1e306823aa71d9bd712e58565148f409
Bastian Mager <bastian.mager@smail.inf.h-brs.de>
2014-04-27 21:30:24 +0200

    some changes...

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,5 +4,5 @@ CollaborativeBookWriting::Application.config.session_store :cookie_store, key: '
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
+# (create the sessions table with "rails generate session_migration")
 # CollaborativeBookWriting::Application.config.session_store :active_record_store
